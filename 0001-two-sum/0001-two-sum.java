22b77ee454c7c409118466235b18c98355f1fa15
Sanjeet Kumar <39269529+heysanjeet@users.noreply.github.com>
2024-01-26 13:35:22 +0530

    Time: 2 ms (91.92%), Space: 44.9 MB (32.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    //Time O(n) | space O(1)
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> prevMap=new HashMap<>();
         for(int i=0;i<nums.length;i++){
@@ -10,4 +11,15 @@ class Solution {
         }
         return new int[]{};
     }
+  // time O(n^2) | space O(1)
+  public int[] twoSumNaive(int[] nums, int target){
+      for(int i=0;i<nums.length;i++){
+          for(int j=i+1;j<nums.length;j++){
+              if(nums[i]+nums[j]==target){
+                  return new int[]{i,j};
+              }
+          }
+      }
+      return new int[]{};
+  }
 }

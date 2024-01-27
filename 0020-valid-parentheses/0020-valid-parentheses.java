e8d7f84e82c42860b9b2938abfe863523c13b7ea
Sanjeet Kumar <39269529+heysanjeet@users.noreply.github.com>
2024-01-27 19:57:01 +0530

    Time: 1 ms (98.73%), Space: 41.6 MB (25.36%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isValid(String s) {
+    public boolean isValid2(String s) {
         Stack<Character> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -21,5 +21,29 @@ class Solution {
 
     private boolean matching(char a, char b) {
         return ((a == '(' && b == ')') || (a == '{' && b == '}') || (a == '[' && b == ']'));
+    }
+    
+    //loop through each character in the string, 
+    //if the character is an opening parenthesis, push the corresponding closing parenthesis onto the stack
+    //
+    public boolean isValid(String s){
+        Stack<Character> stack=new Stack<Character>();
+        for(char c: s.toCharArray()){
+            if(c=='('){
+                stack.push(')');
+            }else if(c=='{'){
+                stack.push('}');
+            }else if(c=='['){
+                stack.push(']');
+            }else if(stack.isEmpty() || stack.pop() !=c){
+               // if the character is a closing bracket
+                // if the stack is empty (i.e., there is no matching opening bracket) or the top of the stack
+                // does not match the closing bracket, the string is not valid, so return false
+                return false;
+            }
+        }
+         // if the stack is empty, all opening brackets have been matched with their corresponding closing brackets,
+        // so the string is valid, otherwise, there are unmatched opening brackets, so return false
+        return stack.isEmpty();
     }
 }
